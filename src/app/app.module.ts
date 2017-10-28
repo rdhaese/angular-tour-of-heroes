e00c94e6dec47b5b19f61f90d3024f3f2d923a59
Robin D'Haese <r.dhaese92@gmail.com>
2017-10-28 16:43:43 +0200

    commit before starting observer part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
- 
-import { AppComponent }        from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './service/in-memory-data.service';
+
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './service/hero.service';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './service/hero.service';
 import { DashboardComponent } from './dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
- 
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
@@ -25,7 +32,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     HeroService
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit} from '@angular/core';
-import {Hero } from './model/hero';
-import {HeroService} from './service/hero.service';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './model/hero';
+import { HeroService } from './service/hero.service';
 
 @Component({
     selector: 'my-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls:  ['./dashboard.component.css']
+    styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
-    
+export class DashboardComponent implements OnInit {
+
     ngOnInit(): void {
         this.heroService.getHeroes()
-            .then(heroes => this.heroes = heroes.slice(1, 5));
+            .then(heroesFromService => this.heroes = heroesFromService);
+        this.heroes = this.heroes.slice(1,5);
     }
 
     heroes: Hero[] = [];
 
-    constructor(private heroService: HeroService) {}
-
+    constructor(private heroService: HeroService) { }
 }
